8738e4ae10605314626821467abdf94f6d5c4ed3
Kay Sweebe <kaysweebe@gmail.com>
2023-03-27 10:31:10 -0600

    Add images to home page

--- src/Home/CardTemplate.tsx
+++ src/Home/CardTemplate.tsx
@@ -1,4 +1,5 @@
 import { 
+    Box,
     Button,
     Card,
     CardBody,
@@ -13,30 +14,40 @@ import { useContext } from "react"
 interface CardTemplateProps {
     title: string,
     link: string,
+    image: string, 
     subtitle: string,
     paragraph: string,
     footer: string,
 }
 
 const CardTemplate = (props: CardTemplateProps) => {
-    const {title, link, subtitle, paragraph, footer} = props
+    const {title, link, image, subtitle, paragraph, footer} = props
     const size = useContext(ResponsiveContext);
+    console.log(`image: ${image}`)
+    const imLink = 'hound.png'
 
     return (
         <Card>
+
+                <Box background="light-5">
+                        <img 
+                            alt="cute hound dog" 
+                            src={require('../assets/images/' + image)} 
+                        />
+                </Box>
             <CardHeader pad="medium">
                 <Heading level={2} margin="none">
                     { title }
                 </Heading>
             </CardHeader>
             <CardBody pad="medium">
-                <Paragraph>{ subtitle }</Paragraph>
+                {/* <Paragraph>{ subtitle }</Paragraph> */}
                 <Paragraph maxLines={size === "small" ? 3 : undefined}>
                     { paragraph }
                 </Paragraph>
             </CardBody>
             <CardFooter pad="medium" background="background-contrast">
-8               <Button href={link} label={footer} onClick={() => {}} {...props} />
+               <Button href={link} label={footer} onClick={() => {}} {...props} />
             </CardFooter>
         </Card>
     )
--- src/Home/HomePage.tsx
+++ src/Home/HomePage.tsx
@@ -11,13 +11,15 @@ const content = [
   {
     "title": "Tic-Tac-Toe Hound",
     "link": "/tic-tac-toe-hound",
+    "image": "hound-2.png",
     "subtitle": "Couch play Tic-Tac-Toe",
     "paragraph": "Introducing Tic Tac Toe Hound, the ultimate twist on a classic game! This new version adds a furry companion to the mix, as players must strategically place their X's and O's to win over the heart of a playful hound. With challenging gameplay and adorable graphics, Tic Tac Toe Hound is the perfect game for all ages. So get ready to unleash your competitive side in Tic Tac Toe Hound!",
     "footer": "Play"
   },
   {
     "title": "Connect X",
-    "link": "/",
+    "link": "/connect-x",
+    "image": "connect-x.png",
     "subtitle": "Connect 4, Connect 5, Connect X",
     "paragraph": "Get ready for a thrilling game of Connect X, where you can connect not only 4, but 5 or even an X! With its colorful pieces and strategic gameplay, Connect X will have you on the edge of your seat as you try to outsmart your opponent and connect your pieces in a row before they do. Will you be able to connect 5 in a row or even an X, or will your opponent outmaneuver you and claim victory? Get ready to find out in Connect X!",
     "footer": "Play",
@@ -25,6 +27,7 @@ const content = [
   {
     "title": "Gamer Life",
     "link": "/",
+    "image": "game-life.png",
     "subtitle": "Connect with friends. Share content. Enjoy.",
     "paragraph": "",
     "footer": "Join Now",
@@ -42,6 +45,7 @@ const HomePage = () => {
               <CardTemplate
                 title={c.title}
                 link={c.link}
+                image={c.image}
                 subtitle={c.subtitle}
                 paragraph={c.paragraph}
                 footer={c.footer}
